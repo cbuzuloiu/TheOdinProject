fbb4ac782f0983563d7ecfff7c5eb99251a84ba6
cosmin <cbuzuloiu@gmail.com>
2025-02-23 19:09:59 +0200

    javascript-exercises-main/03_reverseString finished

--- javascript-exercises-main/03_reverseString/reverseString.js
+++ javascript-exercises-main/03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let reverseString = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reverseString = reverseString + string[i];
+  }
 
+  return reverseString;
 };
+
+console.log(reverseString("hello there"));
 
 // Do not edit below this line
 module.exports = reverseString;
