c8863b01f373dfed9993cbf1e10e8b5da64fafa1
cosmin <cbuzuloiu@gmail.com>
2025-02-23 23:12:16 +0200

    dom MDN ex

--- test/script.js
+++ test/script.js
@@ -1,28 +1,46 @@
-function troubleshooting() {
-  const a = 1;
-  const b = 1;
+`use strict`;
 
-  let result;
+const container = document.querySelector("#container");
 
-  // Edit between these lines
-  // =================================
-  result = a + b;
+let content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
 
-  // =================================
+container.appendChild(content);
 
-  return result;
-}
+content = document.createElement("p");
+content.setAttribute("style", "color: red");
+content.textContent = "Hey I'm red!";
+container.appendChild(content);
 
-console.log(troubleshooting());
+content = document.createElement("h3");
+content.setAttribute("style", "color: blue");
+content.textContent = "Hey I'am red!";
+container.appendChild(content);
 
-let number = Number(prompt("enter a number"));
+content = document.createElement("div");
+content.classList.add("div-2");
+container.appendChild(content);
 
-function numberChecker() {
-  if (number === 6) {
-    return true;
-  } else {
-    return false;
-  }
-}
+const containerDiv2 = document.querySelector(".div-2");
+let contentDiv2 = document.createElement("h1");
+contentDiv2.textContent = "I'm in a div";
+containerDiv2.setAttribute(
+  "style",
+  "border: 1px solid black; background-color: pink"
+);
+containerDiv2.appendChild(contentDiv2);
+contentDiv2 = document.createElement("p");
+contentDiv2.textContent = "ME TOO!";
+containerDiv2.appendChild(contentDiv2);
 
-numberChecker();
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
