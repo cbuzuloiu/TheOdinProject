55b0fe61f6401231078215ea17563abf5e2dcaeb
cosmin <cbuzuloiu@gmail.com>
2025-02-23 19:55:06 +0200

    javascript-exercises-main/04_removeFromArray finished

--- javascript-exercises-main/04_removeFromArray/removeFromArray.js
+++ javascript-exercises-main/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  args.forEach((element) => {
+    let targetValueIndex = arr.indexOf(element);
+
+    while (targetValueIndex !== -1) {
+      arr.splice(targetValueIndex, 1);
+      targetValueIndex = arr.indexOf(element);
+    }
+  });
+
+  return arr;
 };
+
+console.log(removeFromArray([1, 2, 2, 3, 4], 5));
 
 // Do not edit below this line
 module.exports = removeFromArray;
