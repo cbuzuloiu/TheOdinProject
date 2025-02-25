f95aec2a099b29204949b7f4c139eb71b48d78a5
cosmin <cbuzuloiu@gmail.com>
2025-02-25 11:27:11 +0200

    16x16 template created using javascript

--- project-3-etch-a-sketch/script.js
+++ project-3-etch-a-sketch/script.js
@@ -1,1 +1,9 @@
 "use strict";
+// --- SELECT ELEMENTS ---
+const selectDrawingBord = document.querySelector(".drawing-bord");
+
+for (let i = 0; i < 256; i++) {
+  const div = document.createElement("div");
+  div.classList.add("card-pixel");
+  selectDrawingBord.appendChild(div);
+}
