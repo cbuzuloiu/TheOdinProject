ec109c598212263fb531e2458af71331b0232000
cosmin <cbuzuloiu@gmail.com>
2025-02-25 11:37:56 +0200

    code refactor

--- project-3-etch-a-sketch/script.js
+++ project-3-etch-a-sketch/script.js
@@ -2,8 +2,12 @@
 // --- SELECT ELEMENTS ---
 const selectDrawingBord = document.querySelector(".drawing-bord");
 
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement("div");
-  div.classList.add("card-pixel");
-  selectDrawingBord.appendChild(div);
+function createPixelCard(n) {
+  for (let i = 0; i < n; i++) {
+    const div = document.createElement("div");
+    div.classList.add("card-pixel");
+    selectDrawingBord.appendChild(div);
+  }
 }
+
+createPixelCard(256);
