080341b0c89be46067313827e24fdca5164a1ce9
cosmin <cbuzuloiu@gmail.com>
2025-02-25 21:15:15 +0200

    operations for two numbers added/ CLI only

--- project-4-calculator/script.js
+++ project-4-calculator/script.js
@@ -15,3 +15,30 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2;
 }
+
+function operate(operand1, operator, operand2) {
+  if (operator === "+") {
+    return add(operand1, operand2);
+  }
+
+  if (operator === "-") {
+    return subtract(operand1, operand2);
+  }
+
+  if (operator === "*") {
+    return multiply(operand1, operand2);
+  }
+
+  if (operator === "/") {
+    return divide(operand1, operand2);
+  }
+}
+
+let operand1;
+let operator;
+let operand2;
+
+console.log(operate(1, "+", 1));
+console.log(operate(1, "-", 1));
+console.log(operate(2, "*", 2));
+console.log(operate(4, "/", 2));
