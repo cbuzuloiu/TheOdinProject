f9dc9429b5440d6d1af628e0a2deb2b1f3b6c9d6
cosmin <cbuzuloiu@gmail.com>
2025-02-25 13:42:16 +0200

    added UI for users to choose grid dimensions

--- project-3-etch-a-sketch/script.js
+++ project-3-etch-a-sketch/script.js
@@ -1,11 +1,13 @@
 "use strict";
 // --- SELECT ELEMENTS ---
 const selectDrawingBord = document.querySelector(".drawing-bord");
+const inputField = document.querySelector("input");
+const btn = document.querySelector("button");
 
 function createPixelCard(n) {
-  if (n > 100) {
-    return alert("n<=100");
-  }
+  //   if (n > 100) {
+  //     return alert("n<=100");
+  //   }
   const cadrPixelWidth = selectDrawingBord.clientWidth / n;
   const cadrPixelHeight = cadrPixelWidth;
 
@@ -24,6 +26,7 @@ function createPixelCard(n) {
     element.style.width = `${cadrPixelWidth}px`;
     element.style.height = `${cadrPixelHeight}px`;
     element.style.border = "1px solid black";
+    element.style.backgroundColor = "white";
 
     element.addEventListener("mouseover", () => {
       element.style.backgroundColor = `#${Math.floor(
@@ -34,3 +37,19 @@ function createPixelCard(n) {
 }
 
 createPixelCard(16);
+
+btn.addEventListener("click", () => {
+  const n = Number(inputField.value);
+  inputField.value = "";
+
+  if (n <= 0) {
+    alert("Grid dimension must be beween 1 and 100");
+  }
+
+  const divSelect = document.querySelectorAll(".drawing-bord > div");
+  divSelect.forEach((element) => {
+    element.remove();
+  });
+
+  createPixelCard(n);
+});
