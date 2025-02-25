7ed84b1f50834a73a96073085a5f27bd8167f010
cosmin <cbuzuloiu@gmail.com>
2025-02-25 12:57:44 +0200

    createPixelCard functionality updated
    The function takes a number from the user and automatically divides the grid into an n * n format, taking into account the fixed size of the ".drawing-bord" window.

--- project-3-etch-a-sketch/script.js
+++ project-3-etch-a-sketch/script.js
@@ -3,11 +3,28 @@
 const selectDrawingBord = document.querySelector(".drawing-bord");
 
 function createPixelCard(n) {
-  for (let i = 0; i < n; i++) {
+  if (n > 100) {
+    return alert("n<=100");
+  }
+  const cadrPixelWidth = selectDrawingBord.clientWidth / n;
+  const cadrPixelHeight = cadrPixelWidth;
+
+  console.log(selectDrawingBord.clientWidth);
+  console.log(cadrPixelWidth);
+  console.log(cadrPixelHeight);
+
+  for (let i = 0; i < n * n; i++) {
     const div = document.createElement("div");
-    div.classList.add("card-pixel");
     selectDrawingBord.appendChild(div);
   }
+
+  const divSelect = document.querySelectorAll(".drawing-bord > div");
+
+  divSelect.forEach((element) => {
+    element.style.width = `${cadrPixelWidth}px`;
+    element.style.height = `${cadrPixelHeight}px`;
+    element.style.border = "1px solid black";
+  });
 }
 
-createPixelCard(256);
+createPixelCard(16);
