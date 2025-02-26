3a622529ceebd4ecb3b611204d0e17334cd7bbe7
cosmin <cbuzuloiu@gmail.com>
2025-02-26 12:31:38 +0200

    basic operations added to the UI

--- project-4-calculator/script.js
+++ project-4-calculator/script.js
@@ -38,7 +38,86 @@ let operand1;
 let operator;
 let operand2;
 
-console.log(operate(1, "+", 1));
-console.log(operate(1, "-", 1));
-console.log(operate(2, "*", 2));
-console.log(operate(4, "/", 2));
+let operand = "";
+let secondScreenContent = "";
+let operations = [];
+
+const btnDigit = document.querySelectorAll(".digit");
+const btnOperations = document.querySelectorAll(".operations");
+const btnEqual = document.querySelector(".equal");
+const mainScreen = document.querySelector(".container-2");
+const secondScreen = document.querySelector(".container-1");
+
+// mainScreen.textContent = operand;
+
+btnDigit.forEach((element) => {
+  element.addEventListener("click", () => {
+    const content = element.textContent;
+    operand += content;
+    mainScreen.textContent = operand;
+    secondScreenContent += content;
+  });
+});
+
+btnOperations.forEach((element) => {
+  element.addEventListener("click", () => {
+    operations.push(Number(operand));
+    console.log(operations);
+    const content = element.textContent;
+    operations.push(content);
+    console.log(operations);
+    secondScreenContent += ` ${content} `;
+    secondScreen.textContent = secondScreenContent;
+    operand = " ";
+  });
+});
+
+btnEqual.addEventListener("click", () => {
+  operations.push(Number(operand));
+  secondScreen.textContent = secondScreenContent;
+  console.log(operations);
+
+  if (operations[1] === "+") {
+    let result = add(operations[0], operations[2]);
+    mainScreen.textContent = result;
+    operations = [];
+    operand = result;
+    secondScreen.textContent = result;
+    secondScreenContent = result;
+    console.log(operations);
+  }
+
+  if (operations[1] === "-") {
+    let result = subtract(operations[0], operations[2]);
+    mainScreen.textContent = result;
+    operations = [];
+    operand = result;
+    secondScreen.textContent = result;
+    secondScreenContent = result;
+    console.log(operations);
+  }
+
+  if (operations[1] === "X") {
+    let result = multiply(operations[0], operations[2]);
+    mainScreen.textContent = result;
+    operations = [];
+    operand = result;
+    secondScreen.textContent = result;
+    secondScreenContent = result;
+    console.log(operations);
+  }
+
+  if (operations[1] === "/") {
+    let result = divide(operations[0], operations[2]);
+    mainScreen.textContent = result;
+    operations = [];
+    operand = result;
+    secondScreen.textContent = result;
+    secondScreenContent = result;
+    console.log(operations);
+  }
+});
+
+// btn.addEventListener("click", () => {
+//   alert("click");
+// });
